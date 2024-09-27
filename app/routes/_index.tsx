6ec2e7c82fa7863fb291211cdfa37000f171fbe9
adnanfajlur <adnanfajlur@gmail.com>
2024-09-27 21:13:32 +0700

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Remix workshop" },
+    { name: "description", content: "Welcome to Remix workshop!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <p>Hello world</p>
+    </div>
+  );
+}
