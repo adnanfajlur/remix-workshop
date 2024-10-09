ecb450096a1ac79339dd5b6fc4e515488eeac065
adnanfajlur <adnanfajlur@gmail.com>
2024-10-09 19:42:55 +0700

    improve yup-action

--- app/utils/yup-action.ts
+++ app/utils/yup-action.ts
@@ -1,18 +1,33 @@
 import * as yup from 'yup'
 
 export async function yupAction<T extends yup.Schema>(
+	request: FormData,
+	schema: T,
+): Promise<yup.InferType<T>>
+
+export async function yupAction<T extends yup.Schema>(
 	request: Request,
 	schema: T,
-	type: 'form-data' | 'search-params',
-): Promise<yup.InferType<T>> {
+	type?: 'form-data' | 'search-params',
+): Promise<yup.InferType<T>>
+
+export async function yupAction<T extends yup.Schema>(
+	request: Request | FormData,
+	schema: T,
+	type?: 'form-data' | 'search-params',
+) {
 	let data: any = null
 
-	if (type === 'form-data') {
+	if (request instanceof FormData) {
+		data = Object.fromEntries(request)
+	}
+
+	if (type === 'form-data' && request instanceof Request) {
 		const formData = await request.formData()
 		data = Object.fromEntries(formData)
 	}
 
-	if (type === 'search-params') {
+	if (type === 'search-params' && request instanceof Request) {
 		const { searchParams } = new URL(request.url)
 		data = Object.fromEntries(searchParams)
 	}
