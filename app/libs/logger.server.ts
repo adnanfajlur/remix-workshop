41e417903170cfe7c27fce0cccc8cc73ce262e79
adnanfajlur <adnanfajlur@gmail.com>
2024-10-14 04:21:35 +0700

    fix logger

--- app/libs/logger.server.ts
+++ app/libs/logger.server.ts
@@ -1,10 +1,17 @@
 import pino from 'pino'
 
-export const logger = pino({
+const baseConfig = {
+	level: process.env.LOG_LEVEL || 'info',
+}
+
+const devConfig = {
+	...baseConfig,
 	transport: {
 		target: 'pino-pretty',
 		options: {
 			colorize: true,
 		},
 	},
-})
+}
+
+export const logger = pino(process.env.NODE_ENV === 'development' ? devConfig : baseConfig)
